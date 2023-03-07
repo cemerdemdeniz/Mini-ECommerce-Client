4a88d678398c08de86df484e3bba467296fb49c4
Dzhem Erdem <c-e-d@outlook.com.tr>
2023-03-07 01:40:02 +0000

    Added Multiple Layout

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AdminModule } from '../admin.module';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,7 +13,8 @@ import { ComponentsModule } from './components/components.module';
   ],
   imports: [
     CommonModule,
-    ComponentsModule  
+    ComponentsModule  ,
+    RouterModule
   ],
   exports:[
     LayoutComponent
