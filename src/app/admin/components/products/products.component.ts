5556bb72c0e0fc4220c51bd0718d836701f02e77
Dzhem Erdem <c-e-d@outlook.com.tr>
2023-03-20 09:12:42 +0000

    Product Add Process

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
-import { Product } from 'src/app/contracts/product';
+import { Create_Product } from 'src/app/contracts/create_product';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 
 @Component({
@@ -17,31 +17,6 @@ export class ProductsComponent extends BaseComponent implements OnInit{
 
   ngOnInit():void{
     this.showSpinner(SpinnerType.BallScaleMultiple);
-    this.httpClientService.get<Product[]>({
-      controller:"products"
-    }).subscribe(data =>{console.log(data)
-      
-    } );
-
-    // this.httpClientService.post({
-    //   controller:"products"
-    // },{
-    //   name:"Pen",
-    //   stock:100,
-    //   price:15
-    // }).subscribe()
-    // this.httpClientService.put({
-    //   controller : "products",
-    // },{
-    //   id : "dac0e5c5-fbde-42e1-ab38-aee3d81703bd",
-    //   name : "Colorful Pen",
-    //   stock : 500,
-    //   price : 5.5
-    // }).subscribe()
-    // this.httpClientService.delete({
-    //   controller:"products",
-    // },"80744e4a-3bba-4aa6-9bad-a0194bebc8cd").subscribe();
-        
          
    }
 
